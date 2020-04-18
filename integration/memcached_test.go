e5d7857be65425c58ec591ac7af0d443ce0fe3fe
Romano de Souza <romanoaugusto88@gmail.com>
2020-04-18 00:31:25 -0300

    Add benchmarks
    Just to see how slower cachebox is compared to raw storage libraries.

--- integration/memcached_test.go
+++ integration/memcached_test.go
@@ -7,10 +7,14 @@
 package integration
 
 import (
+	"context"
+	"fmt"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/bradfitz/gomemcache/memcache"
+	"github.com/romanodesouza/cachebox"
 	"github.com/romanodesouza/cachebox/storage/memcached"
 )
 
@@ -22,4 +26,112 @@ func TestGoMemcache(t *testing.T) {
 
 	store := memcached.NewGoMemcache(client)
 	run(t, store)
+}
+
+func BenchmarkGoMemcache(b *testing.B) {
+	client := memcache.New(os.Getenv("MEMCACHED_HOST"))
+	store := memcached.NewGoMemcache(client)
+	cache := cachebox.NewCache(store)
+	ctx := context.Background()
+
+	key := "key"
+	ok := []byte("ok")
+
+	b.ResetTimer()
+
+	b.Run("gomemcache:get", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_, _ = client.Get(key)
+		}
+	})
+
+	b.Run("cachebox:get", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_, _ = cache.Get(ctx, key)
+		}
+	})
+
+	b.Run("gomemcache:set", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = client.Set(&memcache.Item{
+				Key:        key,
+				Value:      ok,
+				Expiration: 60,
+			})
+		}
+	})
+
+	b.Run("cachebox:set", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = cache.Set(ctx, cachebox.Item{
+				Key:   key,
+				Value: ok,
+				TTL:   time.Minute,
+			})
+		}
+	})
+
+	b.Run("gomemcache:getmulti", func(b *testing.B) {
+		var keys []string
+		for i := 0; i < b.N; i++ {
+			keys = append(keys, fmt.Sprintf("key_%d", i))
+		}
+		_, _ = client.GetMulti(keys)
+	})
+
+	b.Run("cachebox:getmulti", func(b *testing.B) {
+		var keys []string
+		for i := 0; i < b.N; i++ {
+			keys = append(keys, fmt.Sprintf("key_%d", i))
+		}
+		_, _ = cache.GetMulti(ctx, keys)
+	})
+
+	b.Run("gomemcache:setmulti", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = client.Set(&memcache.Item{
+				Key:        fmt.Sprintf("key_%d", i),
+				Value:      ok,
+				Expiration: 60,
+			})
+		}
+	})
+
+	b.Run("cachebox:setmulti", func(b *testing.B) {
+		var items []cachebox.Item
+		for i := 0; i < b.N; i++ {
+			items = append(items, cachebox.Item{
+				Key:   fmt.Sprintf("key_%d", i),
+				Value: ok,
+				TTL:   time.Minute,
+			})
+		}
+		_ = cache.SetMulti(ctx, items)
+	})
+
+	b.Run("gomemcache:delete", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = client.Delete(key)
+		}
+	})
+
+	b.Run("cachebox:delete", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = cache.Delete(ctx, key)
+		}
+	})
+
+	b.Run("gomemcache:deletemulti", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = client.Delete(fmt.Sprintf("key_%d", i))
+		}
+	})
+
+	b.Run("cachebox:deletemulti", func(b *testing.B) {
+		var keys []string
+		for i := 0; i < b.N; i++ {
+			keys = append(keys, fmt.Sprintf("key_%d", i))
+		}
+		_ = cache.DeleteMulti(ctx, keys)
+	})
 }
--- integration/redis_test.go
+++ integration/redis_test.go
@@ -7,10 +7,14 @@
 package integration
 
 import (
+	"context"
+	"fmt"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/gomodule/redigo/redis"
+	"github.com/romanodesouza/cachebox"
 	storageredis "github.com/romanodesouza/cachebox/storage/redis"
 )
 
@@ -30,4 +34,123 @@ func TestRedigo(t *testing.T) {
 
 	store := storageredis.NewRedigo(pool)
 	run(t, store)
+}
+
+func BenchmarkRedigo(b *testing.B) {
+	pool := &redis.Pool{
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", os.Getenv("REDIS_HOST"))
+		},
+	}
+
+	store := storageredis.NewRedigo(pool)
+	cache := cachebox.NewCache(store)
+	ctx := context.Background()
+
+	key := "key"
+	ok := []byte("ok")
+
+	b.ResetTimer()
+
+	b.Run("redigo:get", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			conn, _ := pool.GetContext(ctx)
+			_, _ = redis.Bytes(conn.Do("GET", key))
+			conn.Close()
+		}
+	})
+
+	b.Run("cachebox:get", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_, _ = cache.Get(ctx, key)
+		}
+	})
+
+	b.Run("redigo:set", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			conn, _ := pool.GetContext(ctx)
+			_, _ = redis.Bytes(conn.Do("SETEX", key, int32(60), ok))
+			conn.Close()
+		}
+	})
+
+	b.Run("cachebox:set", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = cache.Set(ctx, cachebox.Item{
+				Key:   key,
+				Value: ok,
+				TTL:   time.Minute,
+			})
+		}
+	})
+
+	b.Run("redigo:getmulti", func(b *testing.B) {
+		var keys []interface{}
+		for i := 0; i < b.N; i++ {
+			keys = append(keys, fmt.Sprintf("key_%d", i))
+		}
+		conn, _ := pool.GetContext(ctx)
+		_, _ = redis.ByteSlices(conn.Do("MGET", keys...))
+		conn.Close()
+	})
+
+	b.Run("cachebox:getmulti", func(b *testing.B) {
+		var keys []string
+		for i := 0; i < b.N; i++ {
+			keys = append(keys, fmt.Sprintf("key_%d", i))
+		}
+		_, _ = cache.GetMulti(ctx, keys)
+	})
+
+	b.Run("redigo:setmulti", func(b *testing.B) {
+		conn, _ := pool.GetContext(ctx)
+		for i := 0; i < b.N; i++ {
+			_ = conn.Send("SETEX", key, int32(60), ok)
+		}
+		_ = conn.Flush()
+	})
+
+	b.Run("cachebox:setmulti", func(b *testing.B) {
+		var items []cachebox.Item
+		for i := 0; i < b.N; i++ {
+			items = append(items, cachebox.Item{
+				Key:   fmt.Sprintf("key_%d", i),
+				Value: ok,
+				TTL:   time.Minute,
+			})
+		}
+		_ = cache.SetMulti(ctx, items)
+	})
+
+	b.Run("redigo:delete", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			conn, _ := pool.GetContext(ctx)
+			_, _ = conn.Do("DEL", key)
+			conn.Close()
+		}
+	})
+
+	b.Run("cachebox:delete", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = cache.Delete(ctx, key)
+		}
+	})
+
+	b.Run("redigo:deletemulti", func(b *testing.B) {
+		var keys []interface{}
+		for i := 0; i < b.N; i++ {
+			keys = append(keys, fmt.Sprintf("key_%d", i))
+		}
+		conn, _ := pool.GetContext(ctx)
+		_, _ = conn.Do("DEL", keys...)
+		conn.Close()
+	})
+
+	b.Run("cachebox:deletemulti", func(b *testing.B) {
+		var keys []string
+		for i := 0; i < b.N; i++ {
+			keys = append(keys, fmt.Sprintf("key_%d", i))
+		}
+		_ = cache.DeleteMulti(ctx, keys)
+	})
 }
